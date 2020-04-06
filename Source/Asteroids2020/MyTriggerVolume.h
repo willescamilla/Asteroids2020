73858d5c7541bb80670bd72cfb544bbe094d34de
Will <wescamilla10@gmail.com>
2020-04-05 23:57:56 -0400

    Added a boundary that currently displays when an Actor has overlapped

---
+++ Source/Asteroids2020/MyTriggerVolume.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Engine/TriggerVolume.h"
+#include "Asteroids2020Projectile.h"
+#include "MyTriggerVolume.generated.h"
+
+
+UCLASS()
+class ASTEROIDS2020_API AMyTriggerVolume : public ATriggerVolume
+{
+	GENERATED_BODY()
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	// constructor sets default values for this actor's properties
+	AMyTriggerVolume();
+
+	// overlap begin function
+	UFUNCTION()
+		void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
+
+	// overlap end function
+	UFUNCTION()
+		void OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor);
+
+	// specific actor for overlap
+	UPROPERTY(EditAnywhere)
+		class AActor* SpecificActor;
+
+	UPROPERTY(EditAnywhere)
+		class AAsteroids2020Projectile* MyProjectile;
+};
